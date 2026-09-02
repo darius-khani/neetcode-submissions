fec581f70db0be27d68d41f8f6c3390c48ae829e
Darius Khani <123406267+darius-khani@users.noreply.github.com>
2026-09-02 16:58:25 -0400

    Add: anagram-groups - submission-4

---
+++ Data Structures & Algorithms/anagram-groups/submission-4.py
@@ -0,0 +1,18 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        done = [[strs[0]]]
+        checker = {"".join(sorted(strs[0])) : 0}
+        counter = 1
+        print(done)
+        for sub in strs[1:]:
+            sub_s = "".join(sorted(sub))
+            if sub_s in checker:
+                done[checker[sub_s]].append(sub)
+            else:
+                done.append([sub])
+                checker["".join(sorted(sub))] = counter
+                counter += 1
+                #print(done)
+        return done
+                    
+        
